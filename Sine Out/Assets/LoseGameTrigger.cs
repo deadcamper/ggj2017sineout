11858e48158adeb07dfa5e0a199d4a84564c102e
Nate <nw3455@gmail.com>
2017-01-21 15:57:19 -0800

    Add Bottom Trigger

---
+++ Sine Out/Assets/LoseGameTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseGameTrigger : MonoBehaviour {
+
+	public Canvas gameOverScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D collision) {
+		GameObject ball = collision.gameObject;
+		if (ball.tag.Equals("Ball")) {
+			Debug.Log("Ball found.");
+			gameOverScreen.enabled = true;
+		}
+	}
+}
