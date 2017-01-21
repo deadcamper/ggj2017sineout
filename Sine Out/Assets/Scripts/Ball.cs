f0ee29ffcbbefe1f7e2242b097d18681568abb76
deadcamper <ddnewel@gmail.com>
2017-01-21 12:51:18 -0800

    FRIST!
    Break-out, without the blocks to break!

---
+++ Sine Out/Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour {
+
+    public float initialSpeed;
+
+    public Vector2 initialVelocity;
+
+    private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+
+        body.velocity = initialVelocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    /*
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("...");
+
+        Vector2 vel = body.velocity;
+        vel.y = -vel.y;
+        body.velocity = vel;
+    }
+    */
+}
---
+++ Sine Out/Assets/Scripts/PaddleMouseInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMouseInput : MonoBehaviour {
+
+    private Camera mainCamera;
+
+    private float yPosition;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+
+        yPosition = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.mousePresent)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            Vector3 paddlePos = transform.position;
+
+            paddlePos.x = worldMousePos.x;
+            transform.position = paddlePos;
+        }
+    }
+}
