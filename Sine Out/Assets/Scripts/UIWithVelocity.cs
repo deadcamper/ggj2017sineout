98351e554173a35593fde45995874593256dcefd
deadcamper <ddnewel@gmail.com>
2017-01-21 20:59:32 -0800

    Work on the background UI for the game.

---
+++ Sine Out/Assets/Scripts/UIWithVelocity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIWithVelocity : MonoBehaviour {
+
+    public Vector2 velocity;
+
+    RectTransform rect;
+
+	// Use this for initialization
+	void Start () {
+        rect = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 position = rect.position;
+
+        position = position + (Vector3)(velocity) * Time.fixedDeltaTime;
+
+        rect.position = position;
+	}
+}
