ad709e15a5958d47456eabac012544046fe3a2ad
Nate <nw3455@gmail.com>
2017-01-21 23:30:38 -0800

    Add game sounds

--- Sine Out/Assets/Scripts/Ball.cs
+++ Sine Out/Assets/Scripts/Ball.cs
@@ -11,10 +11,6 @@ public class Ball : MonoBehaviour {
     public float sineMultiplier = 1;
 	[FMODUnity.EventRef]
 	public string bounceSound = "event:/Sounds/bounce";
-	[FMODUnity.EventRef]
-	public string music = "event:/Music/Level 1";
-	FMOD.Studio.EventInstance musicEv;                  //cube event music
-	FMOD.Studio.ParameterInstance musicEndParam;        //end param object (for to the end of music)
 
     private Rigidbody2D body;
 	private bool sineWaveTurnedOn = false;
@@ -54,19 +50,6 @@ public class Ball : MonoBehaviour {
     }
 
 	public void setWaveLength(float period,float multiplier) {
-
-		// Start music if it is not already playing
-//		FMOD.Studio.PLAYBACK_STATE play_state;
-//		musicEv.getPlaybackState (out play_state);
-//		if (play_state != FMOD.Studio.PLAYBACK_STATE.PLAYING) {
-//			musicEv.start ();
-//		}
-//
-////		// Set the intensity of the music based on the wave length
-////		if (period < 1) musicEndParam.setValue (0);
-////		if (period > 1 && period < 2) musicEndParam.setValue (1);
-////		if (period >= 2) musicEndParam.setValue (2);
-
 		this.sinePeriod = period;
 		this.sineMultiplier = multiplier;
 	}
---
+++ Sine Out/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	[FMODUnity.EventRef]
+	public string music = "event:/Music/Level 1";
+
+	FMOD.Studio.EventInstance musicEv;                  //cube event music
+	FMOD.Studio.ParameterInstance musicEndParam;        //end param object (for to the end of music)
+
+	// Use this for initialization
+	void Start () {
+		// Start music if it is not already playing
+		FMOD.Studio.PLAYBACK_STATE play_state;
+		musicEv.getPlaybackState (out play_state);
+		if (play_state != FMOD.Studio.PLAYBACK_STATE.PLAYING) {
+			musicEv.start ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeMusic(float period) {
+		// Set the intensity of the music based on the wave length
+		if (period < 1) musicEndParam.setValue (0);
+		if (period > 1 && period < 2) musicEndParam.setValue (1);
+		if (period >= 2) musicEndParam.setValue (2);
+	}
+}
