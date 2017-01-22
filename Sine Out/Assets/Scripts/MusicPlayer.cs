69d5680794c0abaac49a6f4a61322998e8c38438
Nate <nw3455@gmail.com>
2017-01-22 13:49:14 -0800

    Change music based on wave length

--- Sine Out/Assets/Scripts/MusicPlayer.cs
+++ Sine Out/Assets/Scripts/MusicPlayer.cs
@@ -12,42 +12,46 @@ public class MusicPlayer : MonoBehaviour {
 	FMOD.Studio.ParameterInstance fastSpeedParam;
 
 	void Start () {
-		
-		// Start music if it is not already playing	
 		musicEv = FMODUnity.RuntimeManager.CreateInstance(music); 
-		musicEv.getParameter("Slow Speed", out slowSpeedParam);
 		FMOD.Studio.PLAYBACK_STATE play_state;
 		musicEv.getPlaybackState (out play_state);
-		if (play_state != FMOD.Studio.PLAYBACK_STATE.PLAYING) {
-			startMusic ();
-		}
+
+		// Start music at medium
+		changeMusic (1);
 	}
 
 	void Update () {
 		
 	}
 
-	public void startMusic() {
-		slowSpeedParam.setValue (1);
-		musicEv.start ();
-	}
-
 	public void changeMusic(float period) {
 		// Set the intensity of the music based on the wave length
 		if (period < 1) {
+			musicEv.getParameter("Slow Speed", out slowSpeedParam);
 			slowSpeedParam.setValue (1);
 			mediumSpeedParam.setValue (0);
 			fastSpeedParam.setValue (0);
 		}
-		if (period > 1 && period < 2) {
+		if (period >= 1 && period < 2) {
+			musicEv.getParameter("Medium Speed", out slowSpeedParam);
 			slowSpeedParam.setValue (0);
 			mediumSpeedParam.setValue (1);
 			fastSpeedParam.setValue (0);
 		}
 		if (period >= 2) {
+			musicEv.getParameter("Fast Speed", out slowSpeedParam);
 			slowSpeedParam.setValue (0);
 			mediumSpeedParam.setValue (0);
 			fastSpeedParam.setValue (1);
+		}
+
+		startMusic ();
+	}
+
+	public void startMusic() {
+		// Start music if it is not already playing	
+		if (play_state != FMOD.Studio.PLAYBACK_STATE.PLAYING) {
+			musicEv.start ();
 		}
 	}
 }
