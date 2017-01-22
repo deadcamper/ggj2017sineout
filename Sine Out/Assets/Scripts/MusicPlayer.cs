d4091e4e3ad1f4aada9ac506b9930352e8d9b014
Nate <nw3455@gmail.com>
2017-01-22 02:15:37 -0800

    Add background music

--- Sine Out/Assets/Scripts/MusicPlayer.cs
+++ Sine Out/Assets/Scripts/MusicPlayer.cs
@@ -6,13 +6,16 @@ public class MusicPlayer : MonoBehaviour {
 	
 	[FMODUnity.EventRef]
 	public string music = "event:/Music/Level 1";
-	FMOD.Studio.EventInstance musicEv;                  //cube event music
-	FMOD.Studio.ParameterInstance musicEndParam;        //end param object (for to the end of music)
+	FMOD.Studio.EventInstance musicEv;
+	FMOD.Studio.ParameterInstance slowSpeedParam;
+	FMOD.Studio.ParameterInstance mediumSpeedParam;
+	FMOD.Studio.ParameterInstance fastSpeedParam;
 
 	void Start () {
 		
-		// Start music if it is not already playing
+		// Start music if it is not already playing	
 		musicEv = FMODUnity.RuntimeManager.CreateInstance(music); 
+		musicEv.getParameter("Slow Speed", out slowSpeedParam);
 		FMOD.Studio.PLAYBACK_STATE play_state;
 		musicEv.getPlaybackState (out play_state);
 		if (play_state != FMOD.Studio.PLAYBACK_STATE.PLAYING) {
@@ -25,13 +28,26 @@ public class MusicPlayer : MonoBehaviour {
 	}
 
 	public void startMusic() {
+		slowSpeedParam.setValue (1);
 		musicEv.start ();
 	}
 
 	public void changeMusic(float period) {
 		// Set the intensity of the music based on the wave length
-//		if (period < 1) musicEndParam.setValue (0);
-//		if (period > 1 && period < 2) musicEndParam.setValue (1);
-//		if (period >= 2) musicEndParam.setValue (2);
+		if (period < 1) {
+			slowSpeedParam.setValue (1);
+			mediumSpeedParam.setValue (0);
+			fastSpeedParam.setValue (0);
+		}
+		if (period > 1 && period < 2) {
+			slowSpeedParam.setValue (0);
+			mediumSpeedParam.setValue (1);
+			fastSpeedParam.setValue (0);
+		}
+		if (period >= 2) {
+			slowSpeedParam.setValue (0);
+			mediumSpeedParam.setValue (0);
+			fastSpeedParam.setValue (1);
+		}
 	}
 }
