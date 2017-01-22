0d1e5479cb87719ff8a2400a2e4d3767b2a14d4e
deadcamper <ddnewel@gmail.com>
2017-01-21 18:55:23 -0800

    Add the sprites. Also add a splash screen start up.

---
+++ Sine Out/Assets/Scripts/SplashFade.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashFade : MonoBehaviour {
+
+    public float fadeInTimeLength = 1f;
+    public float timeBeforeFadeOut = 3f;
+    public float fadeOutTimeLength = 1f;
+    public float timeBeforeNextScene = 1f;
+    public string nextScene = "";
+
+    private bool isFadingIn = false;
+    private bool isFadingOut = false;
+    private float fadeCounter = 0f;
+
+    private Image fade;
+
+	// Use this for initialization
+	void Start () {
+        fade = GetComponent<Image>();
+        isFadingIn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFadingIn)
+        {
+            FadeIn();
+        }
+        else if (isFadingOut)
+        {
+            FadeOut();
+        }
+	}
+
+    void FadeIn()
+    {
+        fadeCounter += Time.deltaTime;
+
+        Color col = fade.color;
+        col.a = Mathf.Clamp(1 - fadeCounter/fadeInTimeLength, 0, 1);
+        fade.color = col;
+
+        if (fadeCounter >= fadeInTimeLength)
+        {
+            fadeCounter = 0f;
+            isFadingIn = false;
+            Invoke("StartFadeOut", timeBeforeFadeOut);
+        }
+    }
+
+    void FadeOut()
+    {
+        fadeCounter += Time.deltaTime;
+
+        Color col = fade.color;
+        col.a = Mathf.Clamp(fadeCounter / fadeOutTimeLength, 0, 1);
+        fade.color = col;
+
+        if (fadeCounter >= fadeOutTimeLength)
+        {
+            fadeCounter = 0f;
+            isFadingOut = false;
+            Invoke("DoNextScene", timeBeforeNextScene);
+        }
+    }
+
+    void StartFadeOut()
+    {
+        isFadingOut = true;
+    }
+
+    void DoNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
